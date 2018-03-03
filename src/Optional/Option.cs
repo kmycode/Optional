91d7518855e96acec4f204957b5942a7a5bc3e91
KMY <tt@kmycode.net>
2018-03-03 11:10:31 +0900

    Refactoring classes and projects and cut net35 support

--- src/Optional/Option.cs
+++ src/Optional/Option.cs
@@ -1,11 +1,9 @@
-﻿using System;
-
-namespace Optional
+﻿namespace Optional
 {
     /// <summary>
     /// Provides a set of functions for creating optional values.
     /// </summary>
-    public static class Option
+    public static partial class Option
     {
         /// <summary>
         /// Wraps an existing value in an Option&lt;T&gt; instance.
@@ -21,64 +19,6 @@ namespace Optional
         /// <returns>An optional containing the specified value.</returns>
         public static Option<T, TException> Some<T, TException>(T value) =>
             new Option<T, TException>(value, default(TException), true);
-
-        /// <summary>
-        /// Wraps an existing value in an Optional&lt;T, TException&gt; instance.
-        /// If an exception is thrown, returns none with exception object.
-        /// </summary>
-        /// <param name="value">The value getter to be wrapped.</param>
-        /// <returns>An optional containing the specified value.</returns>
-        public static Option<T, TException> SomeOrException<T, TException>(Func<T> value)
-            where TException : Exception
-        {
-            try
-            {
-                return value().Some<T, TException>();
-            }
-            catch (TException e)
-            {
-                return None<T, TException>(e);
-            }
-        }
-
-        /// <summary>
-        /// Wraps an existing value in an Optional&lt;T, TException&gt; instance.
-        /// If an exception is thrown, returns none with exception object.
-        /// </summary>
-        /// <param name="value">The value getter to be wrapped.</param>
-        /// <returns>An optional containing the specified value.</returns>
-        public static Option<T, Exception> SomeOrException<T>(Func<T> value) =>
-            SomeOrException<T, Exception>(value);
-
-        /// <summary>
-        /// Wraps an existing value in an Optional&lt;T, TException&gt; instance.
-        /// If an exception is thrown, returns none with exception object.
-        /// If value is null, returns none and has no exception.
-        /// </summary>
-        /// <param name="value">The value getter to be wrapped.</param>
-        /// <returns>An optional containing the specified value.</returns>
-        public static Option<T, Option<TException>> SomeNotNullOrException<T, TException>(Func<T> value)
-            where TException : Exception
-        {
-            try
-            {
-                return value().SomeNotNull(None<TException>());
-            }
-            catch (TException e)
-            {
-                return None<T, Option<TException>>(e.Some());
-            }
-        }
-
-        /// <summary>
-        /// Wraps an existing value in an Optional&lt;T, TException&gt; instance.
-        /// If an exception is thrown, returns none with exception object.
-        /// If value is null, returns none and has no exception.
-        /// </summary>
-        /// <param name="value">The value getter to be wrapped.</param>
-        /// <returns>An optional containing the specified value.</returns>
-        public static Option<T, Option<Exception>> SomeNotNullOrException<T>(Func<T> value) =>
-            SomeNotNullOrException<T, Exception>(value);
 
         /// <summary>
         /// Creates an empty Option&lt;T&gt; instance.
--- src/Optional/OptionCollectionExtensions.cs
+++ src/Optional/OptionCollectionExtensions.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Optional.Collections
+namespace Optional
 {
     public static class OptionCollectionExtensions
     {
